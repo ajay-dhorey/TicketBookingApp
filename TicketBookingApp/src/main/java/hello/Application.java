1049bdf040647629c50b81f227072f5b6c269576
ajay-dhorey <ajayd25@gmail.com>
2017-08-06 15:17:18 +0530

    Update Application.java

--- TicketBookingApp/src/main/java/hello/Application.java
+++ TicketBookingApp/src/main/java/hello/Application.java
@@ -9,6 +9,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//Spring Application class
+
 @SpringBootApplication
 public class Application {
 
